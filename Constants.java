685fe02e543039b2dfc26a337f6a696974659d68
2wYF9vfDS <50259361+2wYF9vfDS@users.noreply.github.com>
2019-05-04 12:20:00 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,11 @@
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+public class Constants {
+
+	static final String userID = "<Plaeholder>";
+	static final String URI = "https://<host>/challenge";
+	static final String CHALLENGE_OUTPUT_PATH = "https://<host>/challenge/output";
+	static final String CHALLENGE_INPUT_PATH = "https://<host>/challenge/input";
+
+}
